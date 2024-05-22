418332dd7cd0239e1502d29024193cd768812185
Roby Tanama <robytanma@gmail.com>
2024-05-23 03:48:05 +0800

    feat: change page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ const Home: NextPage = async () => {
   //   const res = await fetch(`${API_URL}/users`, { method: "GET" });
   //   console.log("roby -> constHome:NextPage= -> data:", await res.json());
 
-  return <div>Hello!</div>;
+  return (
+    <div className="w-full h-full flex flex-col gap-y-4">
+      <p>Hello! My name is Roby Tanama</p>
+      <p>This is the stalker gear!!</p>
+    </div>
+  );
 };
 
 export default Home;
