ec9a3ec9f75583f35a8b2f906156edb8cf9837e3
Roby Tanama <rtanama@c2r2s11.42singapore.sg>
2024-05-23 18:44:40 +0800

    chore: run prettier

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -67,7 +67,7 @@ const Login: NextPage = () => {
                                         <FormControl>
                                             <Input
                                                 placeholder='Type password here'
-												type="password"
+                                                type='password'
                                                 {...field}
                                             />
                                         </FormControl>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { NextPage } from 'next'
 
 const Home: NextPage = async () => {
-    //   const res = await fetch(`${API_URL}/users`, { method: "GET" });
-    //   console.log("roby -> constHome:NextPage= -> data:", await res.json());
-
     return (
         <div className='w-full h-full flex flex-col gap-y-4'>
             <p>Hello! My name is Roby Tanama</p>
